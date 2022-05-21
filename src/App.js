023a2ede9bbaf73460eb2fdb91f1b71128463896
Ephraim19 <machayoephraim@gmail.com>
2022-05-21 21:31:18 +0300

    Addded google login functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import { useState } from "react";
 
 function App() {
+  const [isAuth,setIsAuth] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home setIsAuth={setIsAuth} />} />
+        <Route path="/blog" element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { auth,provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
+
+function Home({ setIsAuth }) {
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth,provider).then((result) => {
+            localStorage.setItem("isAuth",true)
+            setIsAuth(true)
+        })
+    }
+
+  return (
+    <div className='loginPage'>
+        <p>Login with google</p>
+        <button className='login-with-google-btn' onClick={ signInWithGoogle }>
+            Sign in
+        </button>
+    </div>
+
+  )
+}
+
+export default Home
