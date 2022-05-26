2c821119dd515924f74cd957f577f88317269a6e
Ephraim19 <machayoephraim@gmail.com>
2022-05-26 11:11:47 +0300

    Completed the navbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import "./App.css";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Blogs from "./components/Blogs";
+import Category from "./components/Category";
+import BlogDetail from "./components/BlogDetail";
+
+import Layout from "./hocs/Layout";
+
 import { useState } from "react";
 
 function App() {
-  const [isAuth,setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home setIsAuth={setIsAuth} />} />
-        <Route path="/blog" element={<Blogs />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home setIsAuth={setIsAuth} />} />
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
