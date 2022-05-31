4122ca056925fd68cb57fdf2d1745f4f402d4d59
Ephraim19 <57092540+Ephraim19@users.noreply.github.com>
2022-05-31 11:33:14 +0300

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -36,7 +36,7 @@ const csrftoken = getCookie('csrftoken');
     const fetchData = async () => {
       try {
         const res = await axios.post(
-          `http://127.0.0.1:8000/api/blog/api/blog/category`,
+          `https://djangoephu.herokuapp.com/api/blog/category`,
           { category },
           config
         );
